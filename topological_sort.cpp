3c440397d63e2c6992c7bdb4a191b23000ec845a
S94481rth <93481116+S94481rth@users.noreply.github.com>
2022-07-17 16:11:45 +0530

    Update topological_sort.cpp

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -1,83 +1,51 @@
+/******************************************************************************
 
-#include <iostream>
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include<bits/stdc++.h>
+
 using namespace std;
-class Solution{
-  public:
-  bool ans(int n,vector<int> adj[],vector<int> &visited,stack<int> &s,vector<int> &visal)
-  {
-      visal[n]=1;
-      visited[n]=1;
-      for(auto it:adj[n])
-      {
-          if(!visited[it])
-          {
-              if(ans(it,adj,visited,s,visal))return false;
-          }
-          else if(visited[it]==visal[it])
-          {
-              
-              return true;
-          }
-          
-      }
-      visal[n]=0;
-      s.push(n);
-      return false;
-  }
-  vector<int> topoSort(int n,vector<int> adj[])
-  {
-      bool a;
-      stack<int>s;
-      vector<int> visal(n,0);
-      vector<int> answer;
-      vector<int>visited(n,0);
-      for(int i=0;i<n;i++)
-      {
-          if(!visited[i])
-          {
-             a =ans(i,adj,visited,s,visal);
-          }
-      }
-      if(!a)
-      {
-         while(!s.empty())
-         {
-          answer.push_back(s.top());
-          s.pop();
-         } 
-          
-      }
-      else{
-          cout<<"Cycle detected";
-      }
-      
-      return answer;
-  }
-};
-void addEdge(vector < int > adj[], int u, int v) {
-  adj[u].push_back(v);
+
+void dfs(vector<int> adj[],vector<int>& visited,int v,stack<int> & s){
+    visited[v] = 1;
+    for(auto it: adj[v]){
+        if(!visited[it]){
+            dfs(adj,visited,it,s);
+        }
+    }
+    s.push(v);
+    
 }
 
-int main() {
+void topo(vector<int> adj[],vector<int>& visited,int n){
+    stack<int> s;
+    for(int i=0;i<n;i++){
+        if(!visited[i]){
+            dfs(adj,visited,i,s);
+        }
+    }
+    
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+}
 
-  int V = 6;
-
-  vector < int > adj[V];
-    addEdge(adj,5, 2);
-    addEdge(adj,5, 0);
-    addEdge(adj,4, 0);
-    addEdge(adj,4, 1);
-    addEdge(adj,2, 3);
-    addEdge(adj,3, 1);
-
-  Solution obj;
-  vector < int > res = obj.topoSort(6, adj);
-
-  
-  for (int i = 0; i < res.size(); i++) {
-    cout << res[i] << " ";
-  }
-
-  return 0;
-} 
+int main()
+{
+    int n,m,u,v;
+    cout<<"Enter number of vertices and edges : ";
+    cin>>n>>m;
+    vector<int> adj[n];
+    for(int i=0;i<m;i++){
+        cin>>u>>v;
+        adj[u].push_back(v);
+    }
+    vector<int> visited(n,0);
+    topo(adj,visited,n);
+    return 0;
+}
